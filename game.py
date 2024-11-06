1fc095ac0577cd25e032a2da218bd82b6a1647bd
Mizu-Fer <mizufer7405@gmail.com>
2024-11-06 03:33:19 -0300

    cambios en imagenes.py y solucion sprite no toca los bordes

--- game.py
+++ game.py
@@ -20,18 +20,36 @@ black = 0, 0, 0
 class jugador:
     imagen = pygame.image.load("jugador.png").convert_alpha()
     imagenespepe = imagenes.spritesheet(imagen)    
-    #cambio de imagen
-    frame_0 = imagenespepe.get_image( 0, 48, 48, 3, black)
-    frame_1 = imagenespepe.get_image( 1, 48, 48, 3, black)
+    
+    #animación de imagen
+    animation_list_idle = []
+    animation_list_walk = []
+    animation_steps = 2
+    
+    #agregar imagenes en las listas de animacion
+    for x in range(animation_steps):
+        animation_list_idle.append(imagenespepe.get_image( x, 0, 48, 48, 3, black))
+    for x in range(animation_steps):
+        animation_list_walk.append(imagenespepe.get_image( x + 2, 0, 48, 48, 3, black))
     
     #frame_0 usado como molde xq tienen el mismo tamaño
-    imagenrect = frame_0.get_rect()
-    speed = [1, 1]
+    imagenrect = animation_list_idle[0].get_rect()
+    movimiento_x = 0
+    movimiento_y = 0
     def move_forward():
         imagenrect = imagenrect.move(speed)
+        
+    #sur = 0 , norte
+    def cambiar_direccion():
+        pass
+        
 
 pepe = jugador()
 pepe.imagenrect.move_ip(300,100)
+#actualizar animacion
+last_update = pygame.time.get_ticks()
+animation_coldown = 300
+frame = 0
 #color pantalla
 color = 70, 170, 200
 #empezar bucle
@@ -42,22 +60,35 @@ while run:
         #si es salir de la ventana terminamos
         if event.type == pygame.QUIT: run = False
     
-    #movement(pepe.imagenrect)
+    #colición
     keys = pygame.key.get_pressed() 
-    if keys[pygame.K_UP] and pepe.imagenrect.top > 0:
-        pepe.imagenrect = pepe.imagenrect.move(0,-1)
-    if keys[pygame.K_DOWN] and pepe.imagenrect.bottom < 700:
-        pepe.imagenrect = pepe.imagenrect.move(0,1)
-    if keys[pygame.K_LEFT] and pepe.imagenrect.left > 0:
-        pepe.imagenrect = pepe.imagenrect.move(-1,0)
-    if keys[pygame.K_RIGHT] and pepe.imagenrect.right < 1000:
-        pepe.imagenrect = pepe.imagenrect.move(1,0) 
+    # 40 es la cantidad de pixeles que hay entre el sprite y los bordes de colición
+    if keys[pygame.K_UP] and pepe.imagenrect.top > -40:
+        #pepe.imagenrect = pepe.imagenrect.move(0,-1)
+        pepe.movimiento_y = -1
+    if keys[pygame.K_DOWN] and pepe.imagenrect.bottom < height + 40:
+        #pepe.imagenrect = pepe.imagenrect.move(0,1)
+        pepe.movimiento_y = 1
+    if keys[pygame.K_LEFT] and pepe.imagenrect.left > -40:
+        #pepe.imagenrect = pepe.imagenrect.move(-1,0)
+        pepe.movimiento_x = -1
+    if keys[pygame.K_RIGHT] and pepe.imagenrect.right < width + 40:
+        #pepe.imagenrect = pepe.imagenrect.move(1,0)
+        pepe.movimiento_x = 1 
     
-    #colisión
-
-
-
+    
+    #actualizar movmiento
+    pepe.imagenrect = pepe.imagenrect.move(pepe.movimiento_x, pepe.movimiento_y)
+    pepe.movimiento_x = 0
+    pepe.movimiento_y = 0
+    
+    
     screen.fill(color)
-    screen.blit(pepe.frame_1, pepe.imagenrect)
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= animation_coldown:
+        frame += 1
+        frame = frame % 2
+        last_update = current_time
+    screen.blit(pepe.animation_list_idle[frame], pepe.imagenrect)
     pygame.display.flip()
 pygame.QUIT
--- imagenes.py
+++ imagenes.py
@@ -3,9 +3,9 @@ class spritesheet():
     def __init__(self, image):
         self.sheet = image
     #función para obtener una imagen
-    def get_image (self, frame, width, height, scale, colour):
+    def get_image (self, frame, line, width, height, scale, colour):
         image = pygame.Surface((width, height)).convert_alpha()
-        image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
+        image.blit(self.sheet, (0, 0), ((frame * width), (line * height), width, height))
         image = pygame.transform.scale(image, (width * scale, height * scale))
         image.set_colorkey(colour)
         return image
