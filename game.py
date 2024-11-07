39625389ba85ddb9f13ad131cf55fa06510b360f
Mizu-Fer <mizufer7405@gmail.com>
2024-11-07 01:07:05 -0300

    Jugador animado

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 import imagenes
+import jugador
 
 pygame.init()
 width = 1000
@@ -12,40 +13,8 @@ screen = pygame.display.set_mode((width, height))
 #nombre de ventana
 pygame.display.set_caption("juego de prueba")
 
+pepe = jugador.player()
 
-#transparencia de imagen
-black = 0, 0, 0
-        
-#crear variables
-class jugador:
-    imagen = pygame.image.load("jugador.png").convert_alpha()
-    imagenespepe = imagenes.spritesheet(imagen)    
-    
-    #animación de imagen
-    animation_list_idle = []
-    animation_list_walk = []
-    animation_steps = 2
-    
-    #agregar imagenes en las listas de animacion
-    for x in range(animation_steps):
-        animation_list_idle.append(imagenespepe.get_image( x, 0, 48, 48, 3, black))
-    for x in range(animation_steps):
-        animation_list_walk.append(imagenespepe.get_image( x + 2, 0, 48, 48, 3, black))
-    
-    #frame_0 usado como molde xq tienen el mismo tamaño
-    imagenrect = animation_list_idle[0].get_rect()
-    movimiento_x = 0
-    movimiento_y = 0
-    def move_forward():
-        imagenrect = imagenrect.move(speed)
-        
-    #sur = 0 , norte
-    def cambiar_direccion():
-        pass
-        
-
-pepe = jugador()
-pepe.imagenrect.move_ip(300,100)
 #actualizar animacion
 last_update = pygame.time.get_ticks()
 animation_coldown = 300
@@ -60,28 +29,54 @@ while run:
         #si es salir de la ventana terminamos
         if event.type == pygame.QUIT: run = False
     
+    pepe.caminar = False
+    aceleracion = 5
+    
     #colición
     keys = pygame.key.get_pressed() 
     # 40 es la cantidad de pixeles que hay entre el sprite y los bordes de colición
     if keys[pygame.K_UP] and pepe.imagenrect.top > -40:
         #pepe.imagenrect = pepe.imagenrect.move(0,-1)
         pepe.movimiento_y = -1
+        pepe.cambiar_direccion(1)
+        pepe.caminar = True
+
     if keys[pygame.K_DOWN] and pepe.imagenrect.bottom < height + 40:
         #pepe.imagenrect = pepe.imagenrect.move(0,1)
         pepe.movimiento_y = 1
+        pepe.cambiar_direccion(0)
+        pepe.caminar = True
+
+         
+    if keys[pygame.K_RIGHT] and pepe.imagenrect.right < width + 40:
+        #pepe.imagenrect = pepe.imagenrect.move(1,0)
+        pepe.movimiento_x = 1
+        pepe.cambiar_direccion(3)
+        pepe.caminar = True
+        
     if keys[pygame.K_LEFT] and pepe.imagenrect.left > -40:
         #pepe.imagenrect = pepe.imagenrect.move(-1,0)
         pepe.movimiento_x = -1
-    if keys[pygame.K_RIGHT] and pepe.imagenrect.right < width + 40:
-        #pepe.imagenrect = pepe.imagenrect.move(1,0)
-        pepe.movimiento_x = 1 
+        pepe.cambiar_direccion(2)
+        pepe.caminar = True
+
+    #jugador corre y animación acelera
+    if keys[pygame.K_SPACE]:
+        aceleracion = 2
+        animation_coldown = 150
+    else:
+        animation_coldown = 300
+     
+ 
     
-    
+    if pepe.movimiento_x != 0 or pepe.movimiento_y != 0:
     #actualizar movmiento
-    pepe.imagenrect = pepe.imagenrect.move(pepe.movimiento_x, pepe.movimiento_y)
-    pepe.movimiento_x = 0
-    pepe.movimiento_y = 0
-    
+        pepe.imagenrect = pepe.imagenrect.move(pepe.movimiento_x, pepe.movimiento_y)
+        pepe.movimiento_x = 0
+        pepe.movimiento_y = 0
+        
+        #delay al movimiento del personaje
+        pygame.time.delay(aceleracion)
     
     screen.fill(color)
     current_time = pygame.time.get_ticks()
@@ -89,6 +84,10 @@ while run:
         frame += 1
         frame = frame % 2
         last_update = current_time
-    screen.blit(pepe.animation_list_idle[frame], pepe.imagenrect)
+        
+    if pepe.caminar:
+        screen.blit(pepe.animation_list_walk[frame], pepe.imagenrect)
+    else:
+        screen.blit(pepe.animation_list_idle[frame], pepe.imagenrect)
     pygame.display.flip()
 pygame.QUIT
--- jugador.py
+++ jugador.py
@@ -1,3 +1,36 @@
+import pygame
+import imagenes
 
 class player():
-    pass
+    def __init__(self):
+        imagen = pygame.image.load("jugador.png").convert_alpha()
+        self.imagenespepe = imagenes.spritesheet(imagen)    
+    
+        self.movimiento_x = 0
+        self.movimiento_y = 0
+        self.direccion = 0
+        self.caminar = False
+        
+        #animación de imagen
+        self.animation_list_idle = []
+        self.animation_list_walk = []
+        self.animation_steps = 2
+        
+        self.black = 0, 0, 0
+        #agregar imagenes en las listas de animacion
+        for x in range(self.animation_steps):
+            self.animation_list_idle.append(self.imagenespepe.get_image( x, 0, 48, 48, 3, self.black))
+        for x in range(self.animation_steps):
+            self.animation_list_walk.append(self.imagenespepe.get_image( x + 2, 0, 48, 48, 3, self.black))
+    
+        #frame_0 usado como molde xq tienen el mismo tamaño
+        self.imagenrect = self.animation_list_idle[0].get_rect()
+        self.imagenrect.move_ip(300,100)        
+            
+    #abajo = 0 , arriba = 1, izquierda = 2, derecha = 3
+    def cambiar_direccion(self, una_direccion):
+        for x in range(self.animation_steps):
+            self.animation_list_idle[x] = self.imagenespepe.get_image( x, una_direccion, 48, 48, 3, self.black)
+        for x in range(self.animation_steps):
+            self.animation_list_walk[x] = self.imagenespepe.get_image( x + 2, una_direccion, 48, 48, 3, self.black)
+        
