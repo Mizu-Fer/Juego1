7165484ab1df69ea07d73ed293c412ec68126483
Mizu-Fer <mizufer7405@gmail.com>
2024-11-07 01:47:04 -0300

    ordenar funciones

--- game.py
+++ game.py
@@ -17,10 +17,16 @@ pepe = jugador.player()
 
 #actualizar animacion
 last_update = pygame.time.get_ticks()
-animation_coldown = 300
+
 frame = 0
 #color pantalla
 color = 70, 170, 200
+
+img_start = pygame.image.load("boton_START.PNG").convert_alpha()
+img_exit = pygame.image.load("boton_EXIT.PNG").convert_alpha()
+
+
+
 #empezar bucle
 run = True
 
@@ -30,52 +36,15 @@ while run:
         #si es salir de la ventana terminamos
         if event.type == pygame.QUIT: run = False
     
-    pepe.caminar = False
-    aceleracion = 5
-    
-    #colición
-    keys = pygame.key.get_pressed() 
-    # 40 es la cantidad de pixeles que hay entre el sprite y los bordes de colición
-    if keys[pygame.K_UP] and pepe.imagenrect.top > -40:
-        pepe.movimiento_y = -1
-        pepe.cambiar_direccion(1)
-        pepe.caminar = True
+    #movimiento
+    keys = pygame.key.get_pressed()
+    pepe.movimiento(keys, width, height)
 
-    if keys[pygame.K_DOWN] and pepe.imagenrect.bottom < height + 40:
-        pepe.movimiento_y = 1
-        pepe.cambiar_direccion(0)
-        pepe.caminar = True
-      
-    if keys[pygame.K_RIGHT] and pepe.imagenrect.right < width + 40:
-        pepe.movimiento_x = 1
-        pepe.cambiar_direccion(3)
-        pepe.caminar = True
-        
-    if keys[pygame.K_LEFT] and pepe.imagenrect.left > -40:
-        pepe.movimiento_x = -1
-        pepe.cambiar_direccion(2)
-        pepe.caminar = True
-
-    #jugador corre y animación acelera
-    if keys[pygame.K_SPACE]:
-        aceleracion = 2
-        animation_coldown = 150
-    else:
-        animation_coldown = 300
-     
-         
-    if pepe.movimiento_x != 0 or pepe.movimiento_y != 0:
-    #actualizar movmiento
-        pepe.imagenrect = pepe.imagenrect.move(pepe.movimiento_x, pepe.movimiento_y)
-        pepe.movimiento_x = 0
-        pepe.movimiento_y = 0
-        
-        #delay al movimiento del personaje
-        pygame.time.delay(aceleracion)
-    
+    pepe.actualizar_posicion() 
+  
     screen.fill(color)
     current_time = pygame.time.get_ticks()
-    if current_time - last_update >= animation_coldown:
+    if current_time - last_update >= pepe.animation_coldown:
         frame += 1
         frame = frame % 2
         last_update = current_time
--- jugador.py
+++ jugador.py
@@ -9,12 +9,14 @@ class player():
         self.movimiento_x = 0
         self.movimiento_y = 0
         self.direccion = 0
+        self.aceleracion = 5
         self.caminar = False
         
         #animación de imagen
         self.animation_list_idle = []
         self.animation_list_walk = []
         self.animation_steps = 2
+        self.animation_coldown = 300
         
         self.black = 0, 0, 0
         #agregar imagenes en las listas de animacion
@@ -33,4 +35,44 @@ class player():
             self.animation_list_idle[x] = self.imagenespepe.get_image( x, una_direccion, 48, 48, 3, self.black)
         for x in range(self.animation_steps):
             self.animation_list_walk[x] = self.imagenespepe.get_image( x + 2, una_direccion, 48, 48, 3, self.black)
+    
+    def movimiento(self, keys, limite_ancho, limite_altura):
+        self.caminar = False
+
+        # 40 es la cantidad de pixeles que hay entre el sprite y los bordes de colición
+        if keys[pygame.K_UP] and self.imagenrect.top > -40:
+            self.movimiento_y = -1
+            self.cambiar_direccion(1)
+            self.caminar = True
+
+        if keys[pygame.K_DOWN] and self.imagenrect.bottom < limite_altura + 40:
+            self.movimiento_y = 1
+            self.cambiar_direccion(0)
+            self.caminar = True
         
+        if keys[pygame.K_RIGHT] and self.imagenrect.right < limite_ancho + 40:
+            self.movimiento_x = 1
+            self.cambiar_direccion(3)
+            self.caminar = True
+            
+        if keys[pygame.K_LEFT] and self.imagenrect.left > -40:
+            self.movimiento_x = -1
+            self.cambiar_direccion(2)
+            self.caminar = True    
+            
+        #jugador corre y animación acelera
+        if keys[pygame.K_SPACE]:
+            self.aceleracion = 2
+            self.animation_coldown = 150
+        else:
+            self.animation_coldown = 300
+            self.aceleracion = 5
+        
+    def actualizar_posicion(self):
+        if self.movimiento_x != 0 or self.movimiento_y != 0:
+            #actualizar movmiento
+            self.imagenrect = self.imagenrect.move(self.movimiento_x, self.movimiento_y)
+            self.movimiento_x = 0
+            self.movimiento_y = 0
+            #delay al movimiento del personaje
+            pygame.time.delay(self.aceleracion)
